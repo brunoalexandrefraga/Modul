0eb3ca263dc0bbcc7321d4e4e2ccce56a83dadf4
Bruno Alexandre Fraga <brunoalexandrefraga@gmail.com>
2022-01-28 13:22:13 -0300

    Retirando arquivos compilados do repositório.

--- build/default/production/main.o.d
+++
@@ -1,7 +0,0 @@
-build/default/production/main.o: main.c system.h system_types.h uart1.h
-
-system.h:
-
-system_types.h:
-
-uart1.h:
--- build/default/production/system.o.d
+++
@@ -1,5 +0,0 @@
-build/default/production/system.o: system.c pin_manager.h uart1.h
-
-pin_manager.h:
-
-uart1.h:
